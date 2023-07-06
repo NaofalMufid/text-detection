ff6913f8d2a1cfb666019437ad1343b9dcf3295b
Muh. Naofal Mufid <valnovcc@gmail.com>
2023-07-06 12:12:27 +0700

    add azure ocr

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const Busboy = require('busboy');
 const { textDetect } = require('./awstextract');
+const { OCRAzure } = require('./azureform');
 
 const server = http.createServer(async (req, res) => {
   res.setHeader("Content-Type", "application/json");
@@ -35,7 +36,34 @@ const server = http.createServer(async (req, res) => {
       res.write({ message: err.message })
       res.end();
     });
-    
+
+    req.pipe(busboy);
+  } else if (req.method === 'POST' && req.url === '/azure') {
+    const busboy = Busboy({ headers: req.headers });
+    let fileData, file_name;
+
+    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+      fileData = Buffer.alloc(0);
+      file_name = filename.filename;
+      file.on('data', (chunk) => {
+        fileData = Buffer.concat([fileData, chunk]);
+      });
+    });
+
+    busboy.on('finish', async () => {
+      const aws = await OCRAzure(fileData, file_name);
+      response.status = aws;
+      response.message = 'azure ocr';
+      res.write(JSON.stringify(response));
+      res.end();
+    });
+
+    busboy.on('error', (err) => {
+      res.statusCode(400);
+      res.write({ message: err.message })
+      res.end();
+    });
+
     req.pipe(busboy);
   } else {
     res.write(JSON.stringify(response));
--- azureform.js
+++ azureform.js
@@ -1,19 +1,33 @@
 const { DocumentAnalysisClient, AzureKeyCredential } = require('@azure/ai-form-recognizer');
 const fs = require('fs');
 
-async function OCRAzure(dokumen) {
+async function OCRAzure(dokumen, filename) {
     const endpoint = process.env.AZURE_FORM_ENDPOINT;
     const api_key = process.env.AZURE_API_KEY;
     const model_id = process.env.AZURE_MODEL_ID;
 
-    const file = fs.createReadStream(dokumen);
+    const file = dokumen;
 
     const client = new DocumentAnalysisClient(endpoint, new AzureKeyCredential(api_key));
-    const poller = await client.beginAnalyzeDocument(model_id, file, {
-        onProgress: ({ status }) => {
-            console.log(`status: ${status}`);
-        }
-    });
+    const poller = await client.beginAnalyzeDocument(model_id, file);
+    const { documents: [document] } = await poller.pollUntilDone();
+
+    if (!document) {
+        throw new Error("Expected at least one document in the result.");
+    }
+
+    console.log(
+        "Extracted document:",
+        document.docType,
+        `(confidence: ${document.confidence || "<undefined>"})`
+    );
+    // console.log("Fields:", document.fields);
+    const result = {};
+    for(const attribute in document.fields){
+        const value = document.fields[attribute].value
+        result[attribute] = value;
+    }
+    fs.writeFileSync(`./hasil/azure-ocr-${filename}.json`, JSON.stringify(result));
 };
 
 module.exports = {
